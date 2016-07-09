aa6b9519bdad55a2bf4cf802e0eb9c5d548b50bd
switmac <marieantoinette.go@gmail.com>
2016-07-09 13:10:36 +0800

    Fix rendering for ReactJS elements

---
+++ contents.jsx
@@ -0,0 +1,7 @@
+ReactDOM.render(
+    <div className="contents">
+        <BoxesBg />
+        <Rainbow />
+    </div>,
+    document.getElementById('container')
+);
--- rainbow-block.jsx
+++ rainbow-block.jsx
@@ -32,21 +32,24 @@ var Cloud = React.createClass({
     }
 });
 
-ReactDOM.render(
-    <RainbowBlock>
-        <div>
-            <div className="rainbow">
-                <RainbowColor color="red" link="#whoAmI" label="WhoAmI?"/>
-                <RainbowColor color="orange" link="#postfolio" label="Portfolio"/>
-                <RainbowColor color="yellow" link="#word" label="Word"/>
-                <RainbowColor color="green" link="#scapes" label="Scapes"/>
-                <RainbowColor color="blue" link="#weather" label="Weather?"/>
-                <RainbowColor color="indigo" link="#interest" label="Interest"/>
-                <RainbowColor color="violet" link="#whoAmI" label="WhoAmI?"/>
-                <RainbowColor color="clear"/>
-            </div>
-            <Cloud />
-        </div>
-    </RainbowBlock>,
-    document.getElementById('container')
-);
+var Rainbow = React.createClass({
+    render: function () {
+        return (
+            <RainbowBlock>
+                <div>
+                    <div className="rainbow">
+                        <RainbowColor color="red" link="#whoAmI" label="WhoAmI?"/>
+                        <RainbowColor color="orange" link="#postfolio" label="Portfolio"/>
+                        <RainbowColor color="yellow" link="#word" label="Word"/>
+                        <RainbowColor color="green" link="#scapes" label="Scapes"/>
+                        <RainbowColor color="blue" link="#weather" label="Weather?"/>
+                        <RainbowColor color="indigo" link="#interest" label="Interest"/>
+                        <RainbowColor color="violet" link="#whoAmI" label="WhoAmI?"/>
+                        <RainbowColor color="clear"/>
+                    </div>
+                    <Cloud />
+                </div>
+            </RainbowBlock>
+        );
+    }
+});
