c72af2436f8c6797630df735702e2aef6129101e
switmac <marieantoinette.go@gmail.com>
2016-07-09 13:41:55 +0800

    Fix rendering in ReactJs

--- contents.jsx
+++ contents.jsx
@@ -1,5 +1,10 @@
 ReactDOM.render(
     <div className="contents">
+        <div className='text-bg'>
+            <div className='text'>
+                <img src='http://image.prntscr.com/image/3bda8ff778074cf7b0a4ee8617124630.png'/>
+            </div>
+        </div>
         <Boxes />
         <Rainbow />
     </div>,
